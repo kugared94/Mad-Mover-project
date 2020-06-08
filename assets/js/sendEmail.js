97711433ee0b471d11b117f57c9be9da95db2cda
muhammad syafiq bin abdul Razak <syafiq_razak@hotmail.com>
2020-06-08 02:19:00 +0000

    fixed all the media responsive

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,14 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail", "syafiq", {
+function sendMail(event) {
+    console.log(event)
+    event.preventDefault();
+    var valid=validation()
+
+    if(valid) {
+        emailjs.send("gmail", "syafiq", {
         "from_name":"syafiq",
         "from_email":"syafiq_razak@hotmail.com",
         "question":"avaiable careers",
-        "from_phone":"+60123245341"})
+        "from_phone":"0123245341"})
     .then(
         function(response) {
             console.log("SUCCESS", response);
@@ -12,5 +17,11 @@ function sendMail(contactForm) {
             console.log("FAILED", error);
         }
     );
+
+    return true
+    }
+
+    alert("Error!")
+    
     return false;
 }
